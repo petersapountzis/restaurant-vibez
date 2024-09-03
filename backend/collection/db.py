296fb2becd64061a79563b52c34f6cb025f1e27d
Ben Frankstein <onlysup12345@gmail.com>
2024-09-02 20:21:24 -0500

    added front end folder

--- backend/collection/db.py
+++ backend/collection/db.py
@@ -4,16 +4,18 @@ import pymysql
 class DBmanager:
     def __init__(self):
         self.conn = pymysql.connect(
-            host = "vibez-database1-instance-1.c74gs48gc437.us-east-1.rds.amazonaws.com",
+            host = "database-1.c74gs48gc437.us-east-1.rds.amazonaws.com",
             port=3306,
             user="admin",
             password="Banker2b!",
             database="restaurants"
         )
+        host = "database-1.c74gs48gc437.us-east-1.rds.amazonaws.com"
         self.cursor = self.conn.cursor()
         # set isolation level to read committed
         self.cursor.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED")
         self.conn.commit()
+        
     def __del__(self):
         self.close()
         print("Connection closed")
@@ -28,3 +30,11 @@ class DBmanager:
         self.cursor.execute("SELECT * FROM restaurant_info")
         rows = self.cursor.fetchall()
         return rows
+    def add_restaurants(self,data):
+        """Add restaurants into restaurant_info table ignoring duplicates"""
+        insert_query = """
+            INSERT IGNORE INTO restaurant_info (name, business_id, address, rating, total_ratings, price, cuisine)
+            VALUES (%s, %s, %s, %s, %s, %s, %s);
+            """
+        self.cursor.executemany(insert_query,data)
+        self.conn.commit()
