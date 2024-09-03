0563087cc65855b97a03284f9aad9dcd961e729b
Ben Frankstein <onlysup12345@gmail.com>
2024-09-02 20:29:33 -0500

    added frontend and swift UI project

---
+++ frontend/vibesApp/vibesApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  vibesApp
+//
+//  Created by Ben Frankstein on 9/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ frontend/vibesApp/vibesApp/vibesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibesAppApp.swift
+//  vibesApp
+//
+//  Created by Ben Frankstein on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct vibesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
